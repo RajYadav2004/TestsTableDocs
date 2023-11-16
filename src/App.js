d59a30272fbb0118b96318372139da49462b7845
Raj Yadav <ry191111989@gmail.com>
2023-11-16 18:08:18 +0530

    Adding View Feature

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+// import { Route, Routes } from 'react-router-dom';
 import TestsDocs from './components/screens/TestsDocs';
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path='/' element={<TestsDocs/>} />
-    </Routes>
+    {/* <Routes>
+    <Route path='/' exect element={<TestsDocs/>} />
+    </Routes> */}
+    <TestsDocs/>
+
     </>
   );
 }
